8d51ecf090afe8add7cc4776d95930253dfe7362
Hipuranyhou <jakub.satoplet@tutanota.com>
2020-01-19 15:44:58 +0100

    Finished rot13

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ Custom tools for basic ciphers
 ### rot13
 usage:
 ```
-rot13 -e STRING1 [STRING2...]
-rot13 -d STRING1 [STRING2...]
+rot13 STRING1 [STRING2...]
 ```
--- rot13/rot13.c
+++ rot13/rot13.c
@@ -1,68 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char decryptLower(char c) {
+int getStrLen(char *str) {
+    int len = 0;
+    while (str[len] != '\0') len++;
+    return len;
+}
+
+char getLower(char c) {
     if (c <= 109) return c + 13;
     if (c > 109) return 97 + (12 - (122 - c));
-    return '-';
+    return '?';
 }
 
-char decryptUpper(char c) {
+char getUpper(char c) {
     if (c <= 77) return c + 13;
     if (c > 77) return 65 + (12 - (90 - c));
-    return '-';
+    return '?';
 }
 
-char decryptIndex(char c) {
-    if (c == ' ') return c;
-    if (c >= 'a' && c <= 'z') return decryptLower(c);
-    if (c >= 'A' && c <= 'Z') return decryptUpper(c);
-    return 'c';
-}
-
-void unRot13(char *line) {
-    int len = 0;
-    while (line[len] != '\0') len++;
-    char *decrypted = (char*)malloc((len + 1) * sizeof(char));
-    for (int i = 0; i < len; ++i) {
-        decrypted[i] = decryptIndex(line[i]);
-    }
-    decrypted[len] = '\0';
-    printf("%s\n", decrypted);
-    free(decrypted);
-    return;
+char getIndex(char c) {
+    if (c >= 'a' && c <= 'z') return getLower(c);
+    if (c >= 'A' && c <= 'Z') return getUpper(c);
+    return c;
 }
 
 void Rot13(char *line) {
-    return;
-}
-
-void printUsage(void) {
-    printf("Usage: rot13 -e STRING1 [STRING2...]\n");
-    printf("       rot13 -d STRING1 [STRING2...]\n");
+    int len = getStrLen(line);
+    char *rotated = (char*)malloc((len + 1) * sizeof(char));
+    for (int i = 0; i < len; ++i) {
+        rotated[i] = getIndex(line[i]);
+    }
+    rotated[len] = '\0';
+    printf("%s\n", rotated);
+    free(rotated);
     return;
 }
 
 int main(int argc, char **argv) {
 
-    if (argc < 3) {
+    if (argc < 2) {
         // Missing string argument
         printf("Missing string argument.\n");
-        printUsage();
-    } else if (argv[1][0] == '-' && argv[1][1] == 'd') {
-        // Decrypt option
-        for (int i = 2; i < argc; ++i) {
-            unRot13(argv[i]);
-        }
-    } else if (argv[1][0] == '-' && argv[1][1] == 'e') {
-        // Encrypt option
-        for (int i = 2; i < argc; ++i) {
+        printf("Usage: rot13 STRING1 [STRING2...]\n");
+    } else {
+        // Rotate each string
+        for (int i = 1; i < argc; ++i) {
             Rot13(argv[i]);
         }
-    } else {
-        // Unknown option
-        printf("Unknown option: %s\n", argv[1]);
-        printUsage();
     }
 
     return 0;
